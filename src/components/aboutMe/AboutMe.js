ee1cd9d4b58e84f0c9264243d7bc5d8b24fa6640
BureikuT <bureikut@hotmail.com>
2020-03-26 14:28:39 -0600

    designed the footer of site

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <div>
+            <div></div>
+            <div className="aboutMe"></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default AboutMe
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="info">
+        <div className="location">
+        <div className="locationImg">
+          <i className="fas fa-map-marker-alt"></i>
+        </div>
+        <div className="cityState">Salt Lake City, Utah</div>
+      </div>
+      <div className="email">
+        <div className="emailImg">
+          <i class="far fa-envelope"></i>
+        </div>
+        <div className="emailAddress"> jason@jasonthebotanist.com</div>
+      </div>
+        </div>
+     
+      <div className="end">
+      <div className="copywrite">Jason W. Baker © All Rights Reserved 2017</div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
