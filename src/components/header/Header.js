a78a59edde035f6f4b0b6ecfd85fd9eb8877be58
BureikuT <bureikut@hotmail.com>
2020-03-25 20:12:43 -0600

    updated the header a bit so future links are at the same visual level

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <div className="header">
       <div className="name">Jason W. Baker - Botanical Consultant</div>
-      <div className="home"> Home</div>
+      <div className="home">
+        {" "}
+        Home
+      </div>
       <div className="options">
         <div className="about">About Me</div>
         <div className="contact">Contact Me</div>
