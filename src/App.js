4cb0d9e668731881ac9d1314a624fa0e4f00a400
BureikuT <bureikut@hotmail.com>
2020-03-30 10:03:51 -0600

    have barebones of the about me page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import AboutMe from './components/aboutMe/AboutMe'
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <AboutMe/>
       <Footer/>
     </div>
   );
